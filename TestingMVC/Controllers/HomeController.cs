14fbfc1e2df4c225cd4d8aac124dd3f96c01d5d2
christine0908 <qing.qing.qwerty@hotmail.com>
2019-09-04 10:02:30 +0800

    first commit(1)

---
+++ TestingMVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestingMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            using(TestingProjectDatabaseEntities dc = new TestingProjectDatabaseEntities())
+            {
+                var events = dc.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+    }
+}
